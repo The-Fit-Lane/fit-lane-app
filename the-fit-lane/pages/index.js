f8084d11a77ef7f0ae1e52aac2de7a025d13102b
Ddiiro <ddiiro92@gmail.com>
2023-04-17 12:38:14 +0300

    Add the fitlane logo on the nav bar of the home page

--- the-fit-lane/pages/index.js
+++ the-fit-lane/pages/index.js
@@ -3,7 +3,10 @@
 export default function HomePage() {
   return (
     <div>
-
+      <nav className="nav">
+        <img className="logo" src="../Public/Images/LandingPage/fitlaneLogo.png" alt="fitlane Logo"></img>
+        
+      </nav>
     </div>
   )
 }

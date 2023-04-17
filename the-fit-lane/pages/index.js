20972b64d1ac4ae98ed49ef606e06f32fabfd234
Ddiiro <ddiiro92@gmail.com>
2023-04-17 12:43:03 +0300

    Add the fitlane links on the nav bar of the home page

--- the-fit-lane/pages/index.js
+++ the-fit-lane/pages/index.js
@@ -5,7 +5,14 @@ export default function HomePage() {
     <div>
       <nav className="nav">
         <img className="logo" src="../Public/Images/LandingPage/fitlaneLogo.png" alt="fitlane Logo"></img>
-        
+        <ul>
+          <li><a href="">Women</a></li>
+          <li><a href="">Men</a></li>
+          <li><a href="">Accessories</a></li>
+          <li><a href="">Gift Sets</a></li>
+          <li><a href="">Shoes</a></li>
+          <li><a href="">Sale</a></li>
+        </ul>
       </nav>
     </div>
   )

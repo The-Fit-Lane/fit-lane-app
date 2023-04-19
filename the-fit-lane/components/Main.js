acf37553645430ce077915203c6022b3b88415e7
Ddiiro <ddiiro92@gmail.com>
2023-04-19 15:01:28 +0300

    Add styling to the Carousel pages

--- the-fit-lane/components/Main.js
+++ the-fit-lane/components/Main.js
@@ -2,7 +2,7 @@ import {items} from "/public/HomePage.json";
 import { Carousel } from "react-bootstrap";
 import {useState} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-// import styles from "/styles/Bootstrap.module.css";
+import styles from "../styles/Bootsrap.module.css";
 
 export default function Main() {
         const { bootstrap } = items;
@@ -13,9 +13,9 @@ export default function Main() {
           return (
            <Carousel activeIndex={index} onSelect={handleSelect}>
              {bootstrap.map((item) => (
-               <Carousel.Item key={item.id} interval={4000}>
+               <Carousel.Item key={item.id} className={styles.itemP} interval={4000}>
                  <img src={item.imageUrl} alt="slides" />
-                 <Carousel.Caption >
+                 <Carousel.Caption className={styles.caption}>
                    <h3>{item.title}</h3>
                    <p>{item.body}</p>
                    <button className="btn btn-danger">Shop Now</button>
